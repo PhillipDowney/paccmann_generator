e213d31145f8530cb8ec7559f55bb40b237adaaf
Phil Downey <phildowney@pd-work-mac.local>
2024-03-06 08:47:48 +1100

    gen  add

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 """Install package."""
+
 from setuptools import setup, find_packages
 import os
 import codecs
@@ -6,36 +7,37 @@ import codecs
 
 def read(rel_path):
     here = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
         return fp.read()
 
 
 def get_version(rel_path):
     for line in read(rel_path).splitlines():
-        if line.startswith('__version__'):
+        if line.startswith("__version__"):
             delim = '"' if '"' in line else "'"
             return line.split(delim)[1]
     else:
-        raise RuntimeError('Unable to find version string.')
+        raise RuntimeError("Unable to find version string.")
 
 
 setup(
-    name='paccmann_generator',
-    version=get_version('paccmann_generator/__init__.py'),
-    description='Multimodal generative models for PaccMann^RL.',
-    long_description=open('README.md').read(),
-    url='https://github.com/PaccMann/paccmann_generator',
-    author='Jannis Born, Matteo Manica, Ali Oskooei, Joris Cadow',
-    author_email=(
-        'jab@zurich.ibm.com, drugilsberg@gmail.com, '
-        'ali.oskooei@gmail.com, joriscadow@gmail.com'
-    ),
+    name="paccmann_generator",
+    version=get_version("paccmann_generator/__init__.py"),
+    description="Multimodal generative models for PaccMann^RL.",
+    long_description=open("README.md").read(),
+    url="https://github.com/PaccMann/paccmann_generator",
+    author="Jannis Born, Matteo Manica, Ali Oskooei, Joris Cadow",
+    author_email=("jab@zurich.ibm.com, drugilsberg@gmail.com, " "ali.oskooei@gmail.com, joriscadow@gmail.com"),
     install_requires=[
-        'numpy', 'pandas', 'matplotlib', 'seaborn',
-        'pytoda @ git+https://git@github.com/PaccMann/paccmann_datasets@0.1.1',
-        'torch>=1.0.0', 'six>=1.12.0',
-        'toxsmi @ git+https://git@github.com/PaccMann/toxsmi'
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "seaborn",
+        "pytoda",
+        "torch>=1.0.0",
+        "six>=1.12.0",
+        "toxsmi==0.0.3",
     ],
-    packages=find_packages('.'),
+    packages=find_packages("."),
     zip_safe=False,
 )
