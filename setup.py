f663cdc0cb8a18b707ca4522f8cb8e92bd4b387c
Phil Downey <Phil.downey1@ibm.com>
2024-03-07 19:47:26 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     version=get_version("paccmann_generator/__init__.py"),
     description="Multimodal generative models for PaccMann^RL.",
     long_description=open("README.md").read(),
-    url="https://github.com/PaccMann/paccmann_generator",
+    url="https://github.com/PhillipDowney/paccmann_generator",
     author="Jannis Born, Matteo Manica, Ali Oskooei, Joris Cadow",
     author_email=("jab@zurich.ibm.com, drugilsberg@gmail.com, " "ali.oskooei@gmail.com, joriscadow@gmail.com"),
     install_requires=[
